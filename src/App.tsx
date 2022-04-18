0bdcd26a1e90fdb9a03868f0d63ac26dc29172a1
Marcio Junior <marcio_ale1@hotmail.com>
2022-04-17 22:34:09 -0300

    feat: Implementação do SideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { SideMenu } from "./shared/components";
 import { AppThemeProvider } from "./shared/contexts";
 
 function App() {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes/>
+        <SideMenu>
+          <AppRoutes/>
+        </SideMenu>
       </BrowserRouter>
     </AppThemeProvider>
   );
